907619e9d66a7d499f49a5d49ce22c2d3193225e
kerem <kerem.ordukaya@ogr.deu.edu.tr>
2025-05-19 23:04:37 +0300

    .

--- bot/command_bot.py
+++ bot/command_bot.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from telegram import Update, BotCommand
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
